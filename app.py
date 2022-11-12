ba4bbb10f53eec3c2b507e279eb9059456f281be
Jack Fetzer <fetzerjack@yahoo.com>
2022-11-12 13:35:12 -0500

    added api

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, jsonify, make_response, Response
+from flask_restful import Resource, Api
+import pandas as pd
+
+app=Flask(__name__)
+
+@app.route('/result')
+def get():
+    df = pd.read_csv('Providers.csv')
+    response = make_response(df.to_json(orient='records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+ 
+if __name__ == '__main__':
+    app.run(debug=True, port=9090)
